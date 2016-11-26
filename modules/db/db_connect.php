526ecc426fc85df47bdf9bc3fecc3fc719b2f8bd
kaewgiggle <kaewgiggle@gmail.com>
2016-11-26 15:30:40 +0700

    Connect to MySQL and retrive Exam list

---
+++ modules/db/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+// (MySQLi Object-oriented)
+$servername = "localhost";
+$username = "root";
+$password = "1234567890";
+$dbname = "exam_project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+?>
---
+++ modules/db/db_disconnect.php
@@ -0,0 +1,7 @@
+<?php
+
+// (MySQLi Object-oriented)
+
+$conn->close();
+
+?>
